d0ea3e10d23a36e340fc432b96ac92b8ba545ca9
lnguptavema <nareshgupta2294@gmail.com>
2025-10-23 15:30:11 +0000

    updaint commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,188 @@ const Header = () => (
   <div className="mainContainer">
     <h1 className="name">Gupta Projects</h1>
     <h1>HTML Projects</h1>
-    <h1>ReactJS Projects</h1> 
+    <h1 className="reactHeading">ReactJS Projects</h1>
     <ul className="ulContainer">
+      <li className="listContainer">
+        <img
+          src="https://s2.ezgif.com/tmp/ezgif-284d95739e3cd4.gif"
+          alt="gif"
+          className="listlogo"
+        />
+        <h1 className="nameProject">Match Game</h1>
+        <div>
+          <p className="livePara">Live Link:</p>
+          <a
+            href="https://matchgame23.ccbp.tech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="linkPara">https://matchgame23.ccbp.tech/</p>
+          </a>
+        </div>
+      </li>{' '}
+      <li className="listContainer">
+        <img
+          className="listlogo"
+          src="https://camo.githubusercontent.com/dbe00f791706ab6bec4563338b85fde9667677f3bf12238aabd028685246c8cd/68747470733a2f2f6173736574732e636362702e696e2f66726f6e74656e642f636f6e74656e742f72656163742d6a732f73746f7077617463682d6f75747075742d76322e676966"
+          alt="gif"
+        />
+        <h1 className="nameProject">Stop Watch</h1>
+        <div>
+          <p className="livePara">Live Link:</p>
+          <a
+            href="https://stopwatchvlng21.ccbp.tech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="linkPara">https://stopwatchvlng21.ccbp.tech/</p>
+          </a>
+        </div>
+      </li>{' '}
+      <li className="listContainer">
+        <img
+          className="listlogo"
+          src="https://camo.githubusercontent.com/e6bf16a4afd7053a40b461f0206a2205275183628215036874960dc59ec5aff7/68747470733a2f2f6173736574732e636362702e696e2f66726f6e74656e642f636f6e74656e742f72656163742d6a732f656d6f6a692d67616d652d6f75747075742d76322e676966"
+          alt="gif"
+        />
+        <h1 className="nameProject">Emoji Game</h1>
+        <div>
+          <p className="livePara">Live Link:</p>
+          <a
+            href="https://emojivlng20.ccbp.tech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="linkPara">https://emojivlng20.ccbp.tech/</p>
+          </a>
+        </div>
+      </li>{' '}
+      <li className="listContainer">
+        <img
+          className="listlogo"
+          src="https://camo.githubusercontent.com/0c2939f5b9c53abb2c1a1e71fa743b7a9888fcab4332ef058e427b66f63d268f/68747470733a2f2f6173736574732e636362702e696e2f66726f6e74656e642f636f6e74656e742f72656163742d6a732f6469676974616c2d74696d65722d6f75747075742e676966"
+          alt="gif"
+        />
+        <h1 className="nameProject">Digital Clock</h1>
+        <div>
+          <p className="livePara">Live Link:</p>
+          <a
+            href="https://clockvlng19.ccbp.tech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="linkPara">https://clockvlng19.ccbp.tech/</p>
+          </a>
+        </div>
+      </li>{' '}
+      <li className="listContainer">
+        <img
+          className="listlogo"
+          src="https://camo.githubusercontent.com/7253156610c80414823ab81f96b4102f3b02de52c0e069158b9e745aee9a05e3/68747470733a2f2f6173736574732e636362702e696e2f66726f6e74656e642f636f6e74656e742f72656163742d6a732f6d6f6e65792d6d616e616765722d6f75747075742e676966"
+          alt="gif"
+        />
+        <h1 className="nameProject">Money Manager</h1>
+        <div>
+          <p className="livePara">Live Link:</p>
+          <a
+            href="https://moneyvlng18.ccbp.tech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="linkPara">https://moneyvlng18.ccbp.tech/</p>
+          </a>
+        </div>
+      </li>{' '}
+      <li className="listContainer">
+        <img
+          className="listlogo"
+          src="https://camo.githubusercontent.com/55e545f64ab676c71faf9e03a3692d29c13d0e9c466f8d6a07d1c4f1a4eba83a/68747470733a2f2f6173736574732e636362702e696e2f66726f6e74656e642f636f6e74656e742f72656163742d6a732f6170706f696e746d656e74732d6170702d6f75747075742e676966"
+          alt="gif"
+        />
+        <h1 className="nameProject">Appointment</h1>
+        <div>
+          <p className="livePara">Live Link:</p>
+          <a
+            href="https://appointment17.ccbp.tech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="linkPara">https://appointment17.ccbp.tech/</p>
+          </a>
+        </div>
+      </li>{' '}
+      <li className="listContainer">
+        <img
+          className="listlogo"
+          src="https://camo.githubusercontent.com/99ce7923b4bfe921ad97edfc89c1393793c3f0205cf7afd1c563059603be914e/68747470733a2f2f6173736574732e636362702e696e2f66726f6e74656e642f636f6e74656e742f72656163742d6a732f636f6d6d656e74732d6170702d6f75747075742d76302e676966"
+          alt="gif"
+        />
+        <h1 className="nameProject">Comments</h1>
+        <div>
+          <p className="livePara">Live Link:</p>
+          <a
+            href="https://commentvlng16.ccbp.tech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="linkPara">https://commentvlng16.ccbp.tech/</p>
+          </a>
+        </div>
+      </li>{' '}
+      <li className="listContainer">
+        <img
+          className="listlogo"
+          src="https://camo.githubusercontent.com/bb43b03e1463e322e84be58b3a9b1b3987985c05693223d0070904daeb3546d9/68747470733a2f2f6173736574732e636362702e696e2f66726f6e74656e642f636f6e74656e742f72656163742d6a732f726576696577732d6170702d6f75747075742d76322e676966"
+          alt="gif"
+        />
+        <h1 className="nameProject">Reviews</h1>
+        <div>
+          <p className="livePara">Live Link:</p>
+          <a
+            href="https://reviewvlng15.ccbp.tech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="linkPara">https://reviewvlng15.ccbp.tech/</p>
+          </a>
+        </div>
+      </li>
+      <li className="listContainer">
+        <img
+          className="listlogo"
+          src="https://camo.githubusercontent.com/737cacba48e1d05483b9354e7e3de27ede7b8c07eb0c43b0ba9ce3d988dc65ba/68747470733a2f2f6173736574732e636362702e696e2f66726f6e74656e642f636f6e74656e742f72656163742d6a732f636f696e2d746f73732d67616d652d6f75747075742e676966"
+          alt="gif"
+        />
+        <h1 className="nameProject">Coin Toss Game</h1>
+        <div>
+          <p className="livePara">Live Link:</p>
+          <a
+            href="	https://tossvlng14.ccbp.tech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="linkPara">https://tossvlng14.ccbp.tech/</p>
+          </a>
+        </div>
+      </li>
+      <li className="listContainer">
+        <img
+          className="listlogo"
+          src="https://assets.ccbp.in/frontend/content/react-js/app-store-output.gif"
+          alt="gif"
+        />
+        <h1 className="nameProject">App Store</h1>
+        <div>
+          <p className="livePara">Live Link:</p>
+          <a
+            href="https://appstorevlng13.ccbp.tech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="linkPara">https://appstorevlng13.ccbp.tech//</p>
+          </a>
+        </div>
+      </li>{' '}
       <li className="listContainer">
         <img
           className="listlogo"
@@ -108,7 +288,7 @@ const Header = () => (
             target="_blank"
             rel="noopener noreferrer"
           >
-            <p className="linkPara">https://deletelistvlng.ccbp.tech/</p>       
+            <p className="linkPara">https://deletelistvlng.ccbp.tech/</p>
           </a>
         </div>
       </li>
